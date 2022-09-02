a93f40a57d51696b0b7479f8c1a201b931ebdbe0
Kenny P <kenparedes27@gmail.com>
2022-09-02 14:31:55 -0400

    Created the Redux store and setup Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+
+const store = configureStore({
+  reducer: {}
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
 
