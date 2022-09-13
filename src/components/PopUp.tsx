9b419ba79962b764e7143f5df2accd47ebc710d5
Kenny P <kenparedes27@gmail.com>
2022-09-13 00:23:18 -0400

    Added "Learn About Novodoro" toggle and content

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -2,26 +2,39 @@ import { useState } from "react"
 
 function PopUp() {
   const [isPopUpVisible, setIsPopUpVisible] = useState(true);
-  const [isDropdownToggled, setIsDropdownToggled] = useState(false);
+  const [isAboutToggled, setIsAboutToggled] = useState(false);
+
+  const onToggleClicked = () => {
+    setIsAboutToggled(!isAboutToggled);
+  }
 
   return (
     <div id="PopUp_wrapper">
       <div id="PopUp_container">
-        <h1>BEFORE YOU START</h1>
-        <p>This app helps you complete tasks and discourages procrastination.</p>
-        <div>
-
-        </div>
+        <h1 style={{textAlign:"center"}}>WELCOME</h1>
+        <p>This app helps you complete tasks and discourages procrastination</p>
+        
         <p>
           Before you start, please hide all references to time in your vicinity,
-          physical and digital, so that you are unaware of the actual time.
+          physical and digital, so that you are unaware of the actual time
         </p>
         <ul>
           <li>Move phones far away and turn on "do not disturb"</li>
-          <li>On Windows</li>
-          <li>On Mac</li>
+          <li>Hide clock on Windows</li>
+          <li>Hide clock on Mac</li>
         </ul>
-        <button>CLOSE</button>
+        <div id="PopUp_toggleAbout" >
+          <button id="PopUp_toggleAboutButton" style={{display: isAboutToggled? "none":"block"}} onClick={onToggleClicked}>
+            LEARN MORE ABOUT NOVODORO
+          </button>
+          <p id="PopUp_toggleAboutContent" style={{display: isAboutToggled? "block":"none"}}>
+            Novodoro follows a Pomodoro structure but consistently changes the 
+            rate of time as the clock ticks. You will never know if the timer 
+            will end early, on time, or late. Such unpredictability keeps you 
+            on edge to finish your task and deters procrastination
+          </p>
+        </div>
+        <button id="PopUp_closeButton">CLOSE</button>
       </div>
     </div>
   )
